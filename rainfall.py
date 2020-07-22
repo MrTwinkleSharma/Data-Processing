ecf710e00cbd7d8a17d25c34fdd7b9a9b0a5a2b2
Mr. Twinkle Sharma <smtwinkle451@gmail.com>
2020-07-22 10:18:14 +0530

    Creating DataFrame using CSV and JSON
    1. read_csv()
    2. read_json()

--- rainfall.py
+++ rainfall.py
@@ -4,13 +4,18 @@ import pandas as pd #importing pandas library for later use as short abbreviatio
 #create a key value pair for dictionary to be passed in Dataframe method
 
 data = { 
-'City Name':pd.series([Badaun, Bareilly ,Gorakhpur ,Lucknow ,Kanpur]),
+'City Name':pd.series(['Badaun', 'Bareilly' , 'Gorakhpur' ,'Lucknow' ,'Kanpur']),
 'Actual': pd.series(['1.8mm', '25.2mm', '25.6mm', '12.9mm', '10mm']),
 'Normal': pd.series(['5.5mm', '7.1mm', '10.5mm', '4.7mm', '3.2mm']),
 }
 
 #format of value of a key is pd.series(alist)
 print("Our DataFrame is :\n")
-df = pd.DataFrame(data)
-print(df)
+df1 = pd.DataFrame(data)
+
+#similiary can create using CSV and JSON
+df2 = pd.read_csv (r'./rain.csv')
+df3 = pd.read_json (r'./rain.json')
+
+print(df1,'\n',df2, '\n',df3)
 #Data Frame is Two dimensinal array like spread sheet
