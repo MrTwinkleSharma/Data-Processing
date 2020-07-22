83d7b4c65fd606b64d45343b0830c6fa27944b25
Mr. Twinkle Sharma <smtwinkle451@gmail.com>
2020-07-22 10:13:20 +0530

    Update rainfall.py

--- rainfall.py
+++ rainfall.py
@@ -4,13 +4,13 @@ import pandas as pd #importing pandas library for later use as short abbreviatio
 #create a key value pair for dictionary to be passed in Dataframe method
 
 data = { 
-'City Name':pd.series([Badaun, Bareilly ,Gorakhpur ,Lucknow ,Kanpur])
-'Actual': pd.series(['1.8mm', '25.2mm', '25.6mm', '12.9mm', '10mm'])
-'Normal': pd.series(['5.5mm', '7.1mm', '10.5mm', '4.7mm', '3.2mm'])
+'City Name':pd.series([Badaun, Bareilly ,Gorakhpur ,Lucknow ,Kanpur]),
+'Actual': pd.series(['1.8mm', '25.2mm', '25.6mm', '12.9mm', '10mm']),
+'Normal': pd.series(['5.5mm', '7.1mm', '10.5mm', '4.7mm', '3.2mm']),
 }
 
 #format of value of a key is pd.series(alist)
 print("Our DataFrame is :\n")
-df = pd.Dataframe(data)
+df = pd.DataFrame(data)
 print(df)
 #Data Frame is Two dimensinal array like spread sheet
