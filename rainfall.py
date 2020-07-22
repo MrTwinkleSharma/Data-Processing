137f116b6a3ed4759031ab89158da87aea507476
Mr. Twinkle Sharma <smtwinkle451@gmail.com>
2020-07-22 17:00:30 +0530

    Biggest Mistake : Function are case sensitive

--- rainfall.py
+++ rainfall.py
@@ -4,9 +4,9 @@ import pandas as pd #importing pandas library for later use as short abbreviatio
 #create a key value pair for dictionary to be passed in Dataframe method
 
 data = { 
-'City Name':pd.series(['Badaun', 'Bareilly' , 'Gorakhpur' ,'Lucknow' ,'Kanpur']),
-'Actual': pd.series(['1.8mm', '25.2mm', '25.6mm', '12.9mm', '10mm']),
-'Normal': pd.series(['5.5mm', '7.1mm', '10.5mm', '4.7mm', '3.2mm']),
+'City Name':pd.Series(['Badaun', 'Bareilly' , 'Gorakhpur' ,'Lucknow' ,'Kanpur']),
+'Actual': pd.Series(['1.8mm', '25.2mm', '25.6mm', '12.9mm', '10mm']),
+'Normal': pd.Series(['5.5mm', '7.1mm', '10.5mm', '4.7mm', '3.2mm']),
 }
 
 #format of value of a key is pd.series(alist)
