4c73f91d8c279dff74b7919f828b5317c38e2a43
Mr. Twinkle Sharma <smtwinkle451@gmail.com>
2020-07-22 18:15:56 +0530

    Data Cleaned
    Filling with zero , removing the NaN and counting the rows

--- rainfall.py
+++ rainfall.py
@@ -19,3 +19,24 @@ df3 = pd.read_json (r'./rain.json')
 
 print(df1,'\n',df2, '\n',df3)
 #Data Frame is Two dimensinal array like spread sheet
+
+#this function cleans all the null to 0 without affecting the original dataframe
+#syntax newdataframe = olddataframe.fillna(0), Enter what you want to fill 
+Cleaned_data = df3.fillna(0)
+print(Cleaned_data)
+#dropna() is used to drop the number which does not have a value
+Cleaned_with_remove_data = df3.dropna(0)
+print(Cleaned_with_remove_data)
+
+# Counting the number of rows that missing number
+count = 0
+for row in df3.iterrows():
+  if any(row.isnull()):
+    count = count+1
+    
+#dataframe.iterrows() and series.isnull() are in pandas library, 1st is to iterate the rows and later is for checking is there any of the tuple is false 
+# any is inbuilt function in python to check if there is a false in An iterable object (list, tuple, dictionary) then return false
+
+print(count) # will going to print 1 for this condition 
+
+    
